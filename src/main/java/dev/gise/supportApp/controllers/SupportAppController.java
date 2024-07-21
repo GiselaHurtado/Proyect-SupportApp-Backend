5c8feb3241f2233f71b8da6f0e8c845c6a76affe
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-07-21 13:26:41 +0200

    controller in construction

--- src/main/java/dev/gise/supportApp/controllers/SupportAppController.java
+++ src/main/java/dev/gise/supportApp/controllers/SupportAppController.java
@@ -1,7 +1,13 @@
 package dev.gise.supportApp.controllers;
 import java.util.List;
-import org.springframework.web.bind.annotation.GetMapping;
+
+// import org.springframework.http.HttpStatusCode;
+// import org.springframework.http.ResponseEntity;
+
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 import dev.gise.supportApp.models.SupportApp;
@@ -23,4 +29,8 @@ public List<SupportApp> index() {
     return services.getAll();
 }
 
+// @PostMapping(path = "/supportapps")
+// public ResponseEntity<SupportApp> create(@RequestBody Request newRequest) {
+//     Request request = services.store(newRequest);
+//     return ResponseEntity.status(HttpStatusCode.valueOf(201)).body(request);
 }
